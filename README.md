b71db8f23610836fca2fca800fd1fb68f96f2c95
X-Epicness <masteraidansmith@gmail.com>
2024-08-09 21:07:00 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3DEngine-DevelopmentBuild
+ Just A Simple Engine That I Plan To Support 3D Rendering For Games And Such
---
+++ src/scripts/main.cpp
@@ -0,0 +1,32 @@
+#include "raylib.h"
+
+#define SCREEN_WIDTH (800)
+#define SCREEN_HEIGHT (450)
+
+#define WINDOW_TITLE "Window title"
+
+int main() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
+    SetTargetFPS(60);
+
+    Texture2D texture = LoadTexture(ASSETS_PATH"test.png"); // Check README.md for how this works
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
+        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
+        DrawTexture(texture, texture_x, texture_y, WHITE);
+
+        const char* text = "3DEngine-Development (Now In Process)";
+        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
+        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, SCREEN_HEIGHT / 2 -text_size.y, 20, BLACK);
+
+        EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
+}
